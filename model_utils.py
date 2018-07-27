d046ffdb5d0924e29402664a5334670bca5e8f51
ssheiba <shimon.sheiba@intel.com>
2018-07-27 19:00:57 +0300

    add files of new repository model_utils doc2vec

---
+++ model_utils.py
@@ -0,0 +1,75 @@
+import torch as tr
+import torch.utils.data as dt
+#import torchtext
+#import spacy
+import gensim
+
+
+class CustomDataset(dt.Dataset):
+
+    """
+    Class for handling data before modeling, pre-process and loading
+    """
+
+    def __init__(self, branch_comments_raw_text_df, branch_comments_features_df, branch_comments_user_profiles_df,
+                 branch_submission_dict, submission_data_dict, branch_deltas_data_dict, embeder):
+        """
+        this method uploads and organize all elements in the data for the different parts of the model
+        """
+
+        # IMPORTANT: all parts of data, are ordered by length of branch - descending, rest of entries are filled with zeros
+
+        # First part of dataset is the raw text embedded vector, df M*N:
+        # M - number of branches, N - maximum number of comments in branch, content - embedded vector of raw text
+        self.branch_comments_raw_text_df = branch_comments_raw_text_df
+
+        # Second part of dataset is the features of the comments, df M*N, content - comment features
+        self.branch_comments_features_df = branch_comments_features_df
+
+        # Third part of dataset is the profiles of the users, df M*N, content - user's features
+        self.branch_comments_user_profiles_df = branch_comments_user_profiles_df
+
+        # Forth part of dataset contains two dictionaries:
+        # 1. {branch index: submission id}
+        # 2. {submission id: [submission text, submission features, submitter profile features]}
+        self.branch_submission_dict = branch_submission_dict
+        self.submission_data_dict = submission_data_dict
+
+        # Fifth part of the dataset is a dictionary: {branch index: [is delta in branch, number of deltas in branch,
+        # [deltas comments location in branch]]}
+        self.branch_deltas_data_dict = branch_deltas_data_dict
+
+        self.embeder = embeder
+
+        pass
+
+    def __getitem__(self, index):
+        """
+        this method takes all elements of a single data point for one iteration of the model, and returns them with
+        it's label. the dataloader will use this method for taking batches in training.
+        :param index: index of data point to be retrieved
+        :return: (data point elements, label)
+        """
+        # TODO: embed comments in branch_comments_raw_text_df , embed submission text
+        X = [self.branch_comments_raw_text_df.iloc[index],self.branch_comments_features_df[index],
+             self.branch_comments_user_profiles_df[index], self.submission_data_dict[self.branch_submission_dict[index]]]
+        y = self.branch_deltas_data_dict[index]
+        return (X, y)
+
+    def __len__(self):
+        """
+
+        :return: size of dataset
+        """
+
+        return len(self.branch_comments_raw_text_df.index)
+
+    def df_to_tensor(self, df):
+        """
+        this method takes a df and returns a tensor of embedded / numerical raw data ready for model input
+        :return: tensor
+        """
+        for row in df.iteritems():
+            vec_test = self.embeder.infer_doc_vector(test)
+
+        return
---
+++ pytorch_playground.py
@@ -0,0 +1,172 @@
+import torch as tr
+import pandas as pd
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+#
+# ################################################ TENSOR GRADIENTS #####################################################
+# ######################################################################################################################
+# x = torch.randn(2, 2)
+# y = torch.randn(2, 2)
+# # By default, user created Tensors have ``requires_grad=False``
+# print(x.requires_grad, y.requires_grad)
+# z = x + y
+# # So you can't backprop through z
+# print(z.grad_fn)
+#
+# # ``.requires_grad_( ... )`` changes an existing Tensor's ``requires_grad``
+# # flag in-place. The input flag defaults to ``True`` if not given.
+# x = x.requires_grad_()
+# y = y.requires_grad_()
+# # z contains enough information to compute gradients, as we saw above
+# z = x + y
+# print(z.grad_fn)
+# # If any input to an operation has ``requires_grad=True``, so will the output
+# print(z.requires_grad)
+#
+# # Now z has the computation history that relates itself to x and y
+# # Can we just take its values, and **detach** it from its history?
+# new_z = z.detach()
+#
+# # ... does new_z have information to backprop to x and y?
+# # NO!
+# print(new_z.grad_fn)
+# # And how could it? ``z.detach()`` returns a tensor that shares the same storage
+# # as ``z``, but with the computation history forgotten. It doesn't know anything
+# # about how it was computed.
+# # In essence, we have broken the Tensor away from its past history
+#
+# # You can also stop autograd from tracking history on Tensors with .requires_grad``=True by wrapping the code block in
+# # ``with torch.no_grad():
+#
+# print(x.requires_grad)
+# print((x ** 2).requires_grad)
+#
+# with torch.no_grad():
+#     print((x ** 2).requires_grad)
+#
+# ######################################################################################################################
+# ######################################################################################################################
+#
+#
+# ############## RELU ###############
+#
+# data = torch.randn(2, 2)
+# print(data)
+# print(F.relu(data))
+#
+# ###################################a
+#
+#
+# ############## SOFTMAX ###############
+# # just a non-linearity, but it is special in that it usually is the last operation done in a network.
+# # This is because it takes in a vector of real numbers and returns a probability distribution
+# # Softmax is also in torch.nn.functional
+# data = torch.randn(5)
+# print(data)
+# print(F.softmax(data, dim=0))
+# print(F.softmax(data, dim=0).sum())  # Sums to 1 because it is a distribution!
+# print(F.log_softmax(data, dim=0))  # theres also log_softmax
+#
+# ####################################
+#
+#
+#
+# T_data = [[[1., 2.], [3., 4.]],
+#           [[5., 6.], [7., 8.]]]
+# T = torch.tensor(T_data, requires_grad=True)
+# print(T[0,1,1].item())
+# print(T.grad_fn)
+#
+# # Tensor factory methods have a ``requires_grad`` flag
+# x = torch.tensor([1., 2., 3], requires_grad=True)
+#
+# # With requires_grad=True, you can still do all the operations you previously
+# # could
+# y = torch.tensor([4., 5., 6], requires_grad=True)
+# z = x + y
+# print(z)
+#
+# # BUT z knows something extra.
+# print(z.grad_fn)
+#
+# # tr.manual_seed(1)
+# # a = tr.rand(3,4,5)
+# #
+# # print(tr.__version__)
+#
+# # rnn = tr.nn.LSTMCell(10, 20)
+# # print("rnn is", rnn)
+# # input = tr.randn(6, 3, 10)
+# # print("input is", input)
+# # hx = tr.randn(3, 20)
+# # print("hx is", hx)
+# # cx = tr.randn(3, 20)
+# # print("cx is", cx)
+# # output = []
+# # for i in range(6):
+# #     hx, cx = rnn(input[i], (hx, cx))
+# #     print("hx is", hx)
+# #     print("cx is", cx)
+# #     output.append(hx)
+# # #     print("output is", output)
+# #
+# # raw_text_df = pd.Series([['sdfsdfsd'],['sdfsg']])
+# # raw_text_tensor = tr.Tensor(raw_text_df.values)
+# # print(raw_text_tensor)
+
+
+# lstm = nn.LSTM(3, 3)  # Input dim is 3, output dim is 3
+# inputs = [torch.randn(1, 3) for _ in range(5)]  # make a sequence of length 5
+# print(inputs)
+
+# csv = "all_submissions_comments_with_label_all_deltalog_final.csv"
+# comments_with_label = pd.read_csv(csv)
+# all_submissions_comments_with_label_all_deltalog_final_comment_body = comments_with_label.comment_body
+# all_submissions_comments_with_label_all_deltalog_final_comment_body = \
+#     all_submissions_comments_with_label_all_deltalog_final_comment_body.str.lstrip('b"')
+# all_submissions_comments_with_label_all_deltalog_final_comment_body = \
+#     all_submissions_comments_with_label_all_deltalog_final_comment_body.str.lstrip("b'")
+# # all_submissions_comments_with_label_all_deltalog_final_comment_body = \
+# #     all_submissions_comments_with_label_all_deltalog_final_comment_body[all_submissions_comments_with_label_all_deltalog_final_comment_body.str.contains("Confirmed") == False]
+# # all_submissions_comments_with_label_all_deltalog_final_comment_body = \
+# #     all_submissions_comments_with_label_all_deltalog_final_comment_body[all_submissions_comments_with_label_all_deltalog_final_comment_body.str.contains("[deleted]") == False]
+# all_submissions_comments_with_label_all_deltalog_final_comment_body.to_csv("all_submissions_comments_with_label_all_deltalog_final_comment_body.csv")
+# print("Eli")
+
+# def evaluate_self_sim(self):
+#     """
+#     sanity check: method checks if the model makes a doc most similar to itself
+#     method prints most, second most, median and least similar vec to a doc
+#     :return:
+#     """
+#
+#     ranks = []
+#     second_ranks = []
+#     doc_iter = 0
+#     for doc_id in range(len(self.train_corpus)):
+#         inferred_vector = self.doc2vec_model.infer_vector(self.train_corpus[doc_id].words)
+#         sims = self.doc2vec_model.docvecs.most_similar([inferred_vector], topn=len(self.doc2vec_model.docvecs))
+#         rank = [docid for docid, sim in sims].index(doc_id)
+#         ranks.append(rank)
+#         second_ranks.append(sims[1])
+#         if doc_iter % 500000 == 0:
+#             print('Document ({}): «{}»\n'.format(doc_id, ' '.join(self.train_corpus[doc_id].words)))
+#             print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % self.doc2vec_model)
+#             for label, index in [('MOST', 0), ('SECOND-MOST', 1),
+#                                  ('MEDIAN', len(sims) // 2), ('LEAST', len(sims) - 1)]:
+#                 print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(self.train_corpus[sims[index][0]].words)))
+#         doc_iter += 1
+#
+#     print("now let's see if the docs are most similar to themselves: ", collections.Counter(ranks))
+
+
+# remove words from corpus
+# words_to_remove_list = ["[deleted]'", '>', "Confirmed", "b'", 'b"', '&amp;#8710;', '&#8710;', '&#916;', '&amp;916;',
+#                         '∆', '!delta', 'Δ', '&delta;']
+
+dtype = torch.float
+device = torch.device("cpu")
+
